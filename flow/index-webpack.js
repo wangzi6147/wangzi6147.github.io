1fcee231cb70be82b12b6990d7947ae8423389ff
Zane Wang <363177773@qq.com>
2016-12-02 23:33:52 -0500

    Better game.

--- flow/index-webpack.js
+++ flow/index-webpack.js
@@ -61,18 +61,28 @@
 
 	var _Spirit2 = _interopRequireDefault(_Spirit);
 
+	var _Enemy = __webpack_require__(5);
+
+	var _Enemy2 = _interopRequireDefault(_Enemy);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	/**
-	 * Created by Zane Wang on 2016/12/2.
-	 */
 	var raf = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
 	    window.setTimeout(callback, 1000 / 60); //每帧1000/60ms
-	};
+	}; /**
+	    * Created by Zane Wang on 2016/12/2.
+	    */
+
 
 	var spirit = void 0;
+	var enemies = [];
+	var enemyCount = 10;
+	var timer = void 0;
+	var holdingTime = 0;
+	var bestTime = 0;
 
 	var canvas = document.getElementById("mainCanvas");
+	var cxt = canvas.getContext("2d");
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 
@@ -87,18 +97,89 @@
 	        x: _Map2.default.width / 2,
 	        y: _Map2.default.height / 2,
 	        radius: 10,
-	        color: "red"
+	        color: 'green',
+	        enemies: enemies
 	    });
 	}
 
+	function initEnemies() {
+	    enemies = [];
+	    for (var i = 0; i < enemyCount; i++) {
+	        var x = Math.random() * _Map2.default.width;
+	        var y = Math.random() * _Map2.default.height;
+	        var vx = Math.random() * 2 - 1;
+	        var vy = Math.random() * 2 - 1;
+	        var speed = Math.random() * 3 + 3;
+	        enemies.push(new _Enemy2.default({
+	            x: x,
+	            y: y,
+	            radius: 10,
+	            color: "red",
+	            vx: vx,
+	            vy: vy,
+	            speed: speed
+	        }));
+	    }
+	}
+
+	function collision(enemy, spirit) {
+	    var diffX = enemy.x - spirit.x;
+	    var diffY = enemy.y - spirit.y;
+	    return Math.hypot(diffX, diffY) < enemy.radius + spirit.radius;
+	}
+
+	function renderTimer() {
+	    cxt.textAlign = 'left';
+	    cxt.textBaseline = 'top';
+	    cxt.strokeStyle = 'white';
+	    cxt.font = 'arial';
+	    cxt.fillStyle = 'white';
+	    cxt.fillText("Time: " + holdingTime + "  Best: " + bestTime, 5, 10);
+	}
 	function animate() {
 	    _Map2.default.render();
+	    renderTimer();
+	    if (spirit.dead) {
+	        reset();
+	    }
 	    spirit.render();
+	    for (var i = 0; i < enemyCount; i++) {
+	        enemies[i].render();
+	        if (collision(enemies[i], spirit)) {
+	            spirit.dead = true;
+	        }
+	    }
 	    raf(animate);
 	}
 
-	function start() {
+	function initTimer() {
+	    holdingTime = 0;
+	    clearTimeout(timer);
+	    var time = function time() {
+	        timer = setTimeout(function () {
+	            holdingTime++;
+	            if (bestTime < holdingTime) {
+	                bestTime = holdingTime;
+	            }
+	            if (holdingTime % 5 === 0) {
+	                for (var i = 0; i < enemies.length; i++) {
+	                    enemies[i].speedUp();
+	                }
+	            }
+	            clearTimeout(timer);
+	            time();
+	        }, 1000);
+	    };
+	    time();
+	}
+	function reset() {
+	    initEnemies();
 	    initSpirit();
+	    initTimer();
+	}
+
+	function start() {
+	    reset();
 	    animate();
 	}
 
@@ -195,18 +276,15 @@
 
 	        var _this = _possibleConstructorReturn(this, (Spirit.__proto__ || Object.getPrototypeOf(Spirit)).call(this, options));
 
-	        if (options) {}
+	        if (options) {
+	            _this.enemies = options.enemies;
+	        }
+	        _this.dead = false;
 	        _this.bind();
 	        return _this;
 	    }
 
 	    _createClass(Spirit, [{
-	        key: 'moveTo',
-	        value: function moveTo(x, y) {
-	            this.x = x;
-	            this.y = y;
-	        }
-	    }, {
 	        key: 'bind',
 	        value: function bind() {
 	            var _this2 = this;
@@ -221,7 +299,6 @@
 	                    e.preventDefault();
 	                    var moveX = e.touches[0].pageX - _this2.touchStartX;
 	                    var moveY = e.touches[0].pageY - _this2.touchStartY;
-	                    console.log(moveX + " " + moveY);
 	                    _this2.moveTo(_this2.x + moveX, _this2.y + moveY);
 	                    _this2.touchStartX = e.touches[0].pageX;
 	                    _this2.touchStartY = e.touches[0].pageY;
@@ -233,15 +310,6 @@
 	                });
 	            }
 	        }
-	    }, {
-	        key: 'render',
-	        value: function render() {
-	            _Map2.default.cxt.fillStyle = this.color;
-	            _Map2.default.cxt.beginPath();
-	            _Map2.default.cxt.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-	            _Map2.default.cxt.closePath();
-	            _Map2.default.cxt.fill();
-	        }
 	    }]);
 
 	    return Spirit;
@@ -251,7 +319,7 @@
 
 /***/ },
 /* 4 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
 
@@ -259,23 +327,128 @@
 	    value: true
 	});
 
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /**
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * Created by Zane Wang on 2016/12/2.
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      */
+
+
+	var _Map = __webpack_require__(2);
+
+	var _Map2 = _interopRequireDefault(_Map);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-	/**
-	 * Created by Zane Wang on 2016/12/2.
-	 */
-	var Point = function Point(options) {
-	    _classCallCheck(this, Point);
+	var Point = function () {
+	    function Point(options) {
+	        _classCallCheck(this, Point);
 
-	    if (options) {
-	        this.x = options.x || 1;
-	        this.y = options.y || 1;
-	        this.radius = options.radius || 10;
-	        this.color = options.color || "red";
+	        if (options) {
+	            this.x = options.x || 1;
+	            this.y = options.y || 1;
+	            this.radius = options.radius || 10;
+	            this.color = options.color || "red";
+	        }
 	    }
-	};
+
+	    _createClass(Point, [{
+	        key: "moveTo",
+	        value: function moveTo(x, y) {
+	            this.x = x;
+	            this.y = y;
+	        }
+	    }, {
+	        key: "render",
+	        value: function render() {
+	            _Map2.default.cxt.fillStyle = this.color;
+	            _Map2.default.cxt.beginPath();
+	            _Map2.default.cxt.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+	            _Map2.default.cxt.closePath();
+	            _Map2.default.cxt.fill();
+	        }
+	    }]);
+
+	    return Point;
+	}();
 
 	exports.default = Point;
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
+	var _Point2 = __webpack_require__(4);
+
+	var _Point3 = _interopRequireDefault(_Point2);
+
+	var _Map = __webpack_require__(2);
+
+	var _Map2 = _interopRequireDefault(_Map);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /**
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                * Created by Zane Wang on 2016/12/2.
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                */
+
+
+	var Enemy = function (_Point) {
+	    _inherits(Enemy, _Point);
+
+	    function Enemy(options) {
+	        _classCallCheck(this, Enemy);
+
+	        var _this = _possibleConstructorReturn(this, (Enemy.__proto__ || Object.getPrototypeOf(Enemy)).call(this, options));
+
+	        if (options) {
+	            _this.vx = options.vx || 1;
+	            _this.vy = options.vy || 1;
+	            _this.speed = options.speed;
+	        }
+	        return _this;
+	    }
+
+	    _createClass(Enemy, [{
+	        key: 'speedUp',
+	        value: function speedUp() {
+	            this.speed++;
+	        }
+	    }, {
+	        key: 'render',
+	        value: function render() {
+	            var nextX = this.x + this.vx * this.speed;
+	            var nextY = this.y + this.vy * this.speed;
+	            if (this.x + this.vx * this.speed < this.radius || this.x + this.vx * this.speed > _Map2.default.width - this.radius) {
+	                this.vx *= -1;
+	            }
+	            if (this.y + this.vy * this.speed < this.radius || this.y + this.vy * this.speed > _Map2.default.height - this.radius) {
+	                this.vy *= -1;
+	            }
+	            this.x = this.x + this.vx * this.speed;
+	            this.y = this.y + this.vy * this.speed;
+	            _get(Enemy.prototype.__proto__ || Object.getPrototypeOf(Enemy.prototype), 'render', this).call(this);
+	        }
+	    }]);
+
+	    return Enemy;
+	}(_Point3.default);
+
+	exports.default = Enemy;
 
 /***/ }
 /******/ ]);

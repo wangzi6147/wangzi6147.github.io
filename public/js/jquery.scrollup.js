1a5af1eb8b27dea4747cb8dbe0b4c14b498db534
Zane Wang <363177773@qq.com>
2018-08-27 18:51:24 -0700

    missing file

--- public/js/jquery.scrollup.js
+++ public/js/jquery.scrollup.js
@@ -1,108 +1,168 @@
-/*! jquery.scrollup v1.0.0, @license MIT */
+/*!
+ * scrollup v2.4.1
+ * Url: http://markgoodyear.com/labs/scrollup/
+ * Copyright (c) Mark Goodyear — @markgdyr — http://markgoodyear.com
+ * License: MIT
+ */
+(function ($, window, document) {
+    'use strict';
 
-(function ( $ ) {
+    // Main function
+    $.fn.scrollUp = function (options) {
 
-  $.fn.scrollup = {}
+        // Ensure that only one scrollUp exists
+        if (!$.data(document.body, 'scrollUp')) {
+            $.data(document.body, 'scrollUp', true);
+            $.fn.scrollUp.init(options);
+        }
+    };
 
-  $.fn.scrollup.create = function(selector, options) {
+    // Init
+    $.fn.scrollUp.init = function (options) {
 
-    options = $(this).scrollup.options(options);
+        // Define vars
+        var o = $.fn.scrollUp.settings = $.extend({}, $.fn.scrollUp.defaults, options),
+            triggerVisible = false,
+            animIn, animOut, animSpeed, scrollDis, scrollEvent, scrollTarget, $self;
 
-    if (options.debug) {
-        console.log('jQuery.scrollup initialised on selector: "'+selector+'"');
-    }
-
-    $(selector).on('wheel', function(e) {
-      if ($(this).scrollTop() == 0 && e.originalEvent.wheelDelta >= 0) {
-
-        if ($(this).children('.' + options.class).length == 0) {
-
-            if (options.debug) { console.log('jQuery.scrollup object is being created.'); }
-            if (options.create) { options.create(); }
-
-            var scrollupElement = $('<div class="'+options.class+'">'+options.content+'</div>');
-
-            scrollupElement.hide().prependTo(selector).show(options.animationSpeed);
-
-            if (options.debug) { console.log('jQuery.scrollup object has been created.'); }
-            if (options.created) { options.created(); }
-
-            if (options.timeout) {
-
-                var timer = setTimeout(function() {
-                    if (options.debug) { console.log('jQuery.scrollup object has timed out.'); }
-                    if (options.timedOut) { options.timedOut(); }
-
-                    $(this).scrollup.remove(selector, options);
-                }, options.timeout);
-
-                scrollupElement.attr('data-scrollup-timer', timer);
-
-            }
-
+        // Create element
+        if (o.scrollTrigger) {
+            $self = $(o.scrollTrigger);
         } else {
-
-            if (options.debug) { console.log('jQuery.scrollup triggered, but object already exists.'); }
-            if (options.exists) { options.exists(); }
-
+            $self = $('<a/>', {
+                id: o.scrollName,
+                href: '#top'
+            });
         }
-      }
-    });
 
-  }
-  
-  
-  
-  $.fn.scrollup.remove = function(selector, options) {
+        // Set scrollTitle if there is one
+        if (o.scrollTitle) {
+            $self.attr('title', o.scrollTitle);
+        }
 
-    options = $(this).scrollup.options(options);
-    
-    if (options.debug) { console.log('jQuery.scrollup object is being removed.'); }
-    if (options.remove) { options.remove(); }
-    
-    var scrollupElement = $(selector).children('.' + options.class);
-    
-    if (scrollupElement.length) {
-        $(scrollupElement).hide(options.animationSpeed, function() {
-            $(scrollupElement).remove();
-            clearTimeout($(scrollupElement).attr('data-scrollup-timer'));
-            if (options.debug) { console.log('jQuery.scrollup object has been removed.'); }
-            if (options.removed) { options.removed(); }
-            return true;
+        $self.appendTo('body');
+
+        // If not using an image display text
+        if (!(o.scrollImg || o.scrollTrigger)) {
+            $self.html(o.scrollText);
+        }
+
+        // Minimum CSS to make the magic happen
+        $self.css({
+            display: 'none',
+            position: 'fixed',
+            zIndex: o.zIndex
         });
-    } else {
-        if (options.debug) {
-            console.log('jQuery.scrollup did not find a scrollup object to remove.');
+
+        // Active point overlay
+        if (o.activeOverlay) {
+            $('<div/>', {
+                id: o.scrollName + '-active'
+            }).css({
+                position: 'absolute',
+                'top': o.scrollDistance + 'px',
+                width: '100%',
+                borderTop: '1px dotted' + o.activeOverlay,
+                zIndex: o.zIndex
+            }).appendTo('body');
         }
-        return false;
-    }
 
-  }
-  
-  
-  
-  $.fn.scrollup.options = function(options) {
+        // Switch animation type
+        switch (o.animation) {
+            case 'fade':
+                animIn = 'fadeIn';
+                animOut = 'fadeOut';
+                animSpeed = o.animationSpeed;
+                break;
 
-    if (typeof options == 'undefined') {
-        options = {};
-    }
+            case 'slide':
+                animIn = 'slideDown';
+                animOut = 'slideUp';
+                animSpeed = o.animationSpeed;
+                break;
 
-    if (typeof options.content == 'undefined') {
-        options.content = '';
-    }
+            default:
+                animIn = 'show';
+                animOut = 'hide';
+                animSpeed = 0;
+        }
 
-    if (typeof options.class == 'undefined') {
-        options.class = 'scrollup';
-    }
+        // If from top or bottom
+        if (o.scrollFrom === 'top') {
+            scrollDis = o.scrollDistance;
+        } else {
+            scrollDis = $(document).height() - $(window).height() - o.scrollDistance;
+        }
 
-    if (typeof options.animationSpeed == 'undefined' || options.animationSpeed.toLowerCase() != 'fast' || options.animationSpeed.toLowerCase() != 'slow') {
-        options.animationSpeed = 'fast';
-    }
-    
-    return options;
+        // Scroll function
+        scrollEvent = $(window).scroll(function () {
+            if ($(window).scrollTop() > scrollDis) {
+                if (!triggerVisible) {
+                    $self[animIn](animSpeed);
+                    triggerVisible = true;
+                }
+            } else {
+                if (triggerVisible) {
+                    $self[animOut](animSpeed);
+                    triggerVisible = false;
+                }
+            }
+        });
 
-  }
-  
+        if (o.scrollTarget) {
+            if (typeof o.scrollTarget === 'number') {
+                scrollTarget = o.scrollTarget;
+            } else if (typeof o.scrollTarget === 'string') {
+                scrollTarget = Math.floor($(o.scrollTarget).offset().top);
+            }
+        } else {
+            scrollTarget = 0;
+        }
 
+        // To the top
+        $self.click(function (e) {
+            e.preventDefault();
 
-}( jQuery ));
+            $('html, body').animate({
+                scrollTop: scrollTarget
+            }, o.scrollSpeed, o.easingType);
+        });
+    };
+
+    // Defaults
+    $.fn.scrollUp.defaults = {
+        scrollName: 'scrollUp',      // Element ID
+        scrollDistance: 300,         // Distance from top/bottom before showing element (px)
+        scrollFrom: 'top',           // 'top' or 'bottom'
+        scrollSpeed: 300,            // Speed back to top (ms)
+        easingType: 'linear',        // Scroll to top easing (see http://easings.net/)
+        animation: 'fade',           // Fade, slide, none
+        animationSpeed: 200,         // Animation in speed (ms)
+        scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
+        scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
+        scrollText: 'Scroll to top', // Text for element, can contain HTML
+        scrollTitle: false,          // Set a custom <a> title if required. Defaults to scrollText
+        scrollImg: false,            // Set true to use image
+        activeOverlay: false,        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+        zIndex: 2147483647           // Z-Index for the overlay
+    };
+
+    // Destroy scrollUp plugin and clean all modifications to the DOM
+    $.fn.scrollUp.destroy = function (scrollEvent) {
+        $.removeData(document.body, 'scrollUp');
+        $('#' + $.fn.scrollUp.settings.scrollName).remove();
+        $('#' + $.fn.scrollUp.settings.scrollName + '-active').remove();
+
+        // If 1.7 or above use the new .off()
+        if ($.fn.jquery.split('.')[1] >= 7) {
+            $(window).off('scroll', scrollEvent);
+
+        // Else use the old .unbind()
+        } else {
+            $(window).unbind('scroll', scrollEvent);
+        }
+    };
+
+    $.scrollUp = $.fn.scrollUp;
+
+})(jQuery, window, document);
